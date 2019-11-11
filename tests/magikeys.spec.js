d524971f792640ab5945be42201baf6ca1dd68f9
betaWeb <cyprien.glepin@gmail.com>
2019-11-11 17:17:27 +0100

    Improve tests

--- tests/magikeys.spec.js
+++ tests/magikeys.spec.js
@@ -1,43 +1,45 @@
+const pm2 = require('pm2')
+const _beforeEach = async options => {
+    this.options = options
+    this.expected_result = null
+    await page.goto(PATH, { waitUntil: 'domcontentloaded' })
+    await page.exposeFunction('onExpected', exp => this.expected_result = exp.join(''));
+    await page.evaluateHandle(options => (
+        new window.Magikeys(
+            options.expected,
+            exp => window.onExpected(exp),
+            {
+                exclude: ['textarea'],
+                context: options.context
+            }
+        )
+    ), this.options)
+}
+const _afterEach = async () => {
+    await jestPuppeteer.resetBrowser()
+}
+
+beforeAll(() => {
+    pm2.start({
+        script: 'npm run serve',
+        autorestart : false
+    }, err => {
+        pm2.disconnect()
+        if (err) throw err
+    })
+})
+
+afterAll(() => {
+    pm2.disconnect()
+})
+
 describe('Magikeys', () => {
 
     describe(' - in BODY context', () => {
-        beforeEach(async () => {
-            this.options = {
-                expected: 'test',
-                context: 'body'
-            }
-            this.expected_result = null
-            await page.goto(PATH, { waitUntil: 'domcontentloaded' })
-            await page.exposeFunction('onExpected', exp => this.expected_result = exp.join(''));
-            this.sequence = await page.evaluateHandle(options => (
-                new window.Magikeys(
-                    options.expected,
-                    exp => window.onExpected(exp),
-                    {
-                        exclude: ['textarea'],
-                        context: options.context
-                    }
-                )
-            ), this.options)
-        })
-
-        test('should have an input with [name="demo"]', async () => {
-            const el = await page.$('[name="demo"]')
-            const tagName = await el.getProperty('tagName')
-            expect(el).not.toBeNull()
-            expect((await tagName.jsonValue()).toLowerCase()).toEqual('input')
-        })
-
-        test('should have a textarea with [name="demo2"]', async () => {
-            const el = await page.$('[name="demo2"]')
-            const tagName = await el.getProperty('tagName')
-            expect(el).not.toBeNull()
-            expect((await tagName.jsonValue()).toLowerCase()).toEqual('textarea')
-        })
-
-        test('should have a valid Magikeys instance', async () => {
-            expect(this.sequence).not.toBeUndefined()
-        })
+        beforeEach(async () => _beforeEach({
+            expected: 'test',
+            context: 'body'
+        }))
 
         test('should trigger the expected sequence', async () => {
             await page.type('body', this.options.expected, 100)
@@ -52,34 +54,16 @@ describe('Magikeys', () => {
             expect(this.expected_result).toBeNull()
         })
 
-        afterEach(async () => {
-            this.sequence = null
-            await jestPuppeteer.resetBrowser()
-        })
+        afterEach(async () => _afterEach())
 
     })
 
     describe(' - in INPUT context', () => {
 
-        beforeEach(async () => {
-            this.options = {
-                expected: 'test',
-                context: '[name="demo"]'
-            }
-            this.expected_result = null
-            await page.goto(PATH, { waitUntil: 'domcontentloaded' })
-            await page.exposeFunction('onExpected', exp => this.expected_result = exp.join(''));
-            this.sequence = await page.evaluateHandle(options => (
-                new window.Magikeys(
-                    options.expected,
-                    exp => window.onExpected(exp),
-                    {
-                        exclude: ['textarea'],
-                        context: options.context
-                    }
-                )
-            ), this.options)
-        })
+        beforeEach(async () => _beforeEach({
+            expected: 'test',
+            context: '[name="demo"]'
+        }))
 
         test('should trigger the expected sequence', async () => {
             await page.type('[name="demo"]', this.options.expected, 100)
@@ -88,34 +72,16 @@ describe('Magikeys', () => {
             expect(this.expected_result).toEqual(this.options.expected)
         })
 
-        afterEach(async () => {
-            this.sequence = null
-            await jestPuppeteer.resetBrowser()
-        })
+        afterEach(async () => _afterEach())
 
     })
 
     describe(' - in TEXTAREA context', () => {
 
-        beforeEach(async () => {
-            this.options = {
-                expected: 'test',
-                context: '[name="demo2"]'
-            }
-            this.expected_result = null
-            await page.goto(PATH, { waitUntil: 'domcontentloaded' })
-            await page.exposeFunction('onExpected', exp => this.expected_result = exp.join(''));
-            this.sequence = await page.evaluateHandle(options => (
-                new window.Magikeys(
-                    options.expected,
-                    exp => window.onExpected(exp),
-                    {
-                        exclude: ['textarea'],
-                        context: options.context
-                    }
-                )
-            ), this.options)
-        })
+        beforeEach(async () => _beforeEach({
+            expected: 'test',
+            context: '[name="demo2"]'
+        }))
 
         test('should not trigger the expected sequence in excluded context', async () => {
             await page.type('[name="demo2"]', this.options.expected, 100)
@@ -123,10 +89,7 @@ describe('Magikeys', () => {
             expect(this.expected_result).toBeNull()
         })
 
-        afterEach(async () => {
-            this.sequence = null
-            await jestPuppeteer.resetBrowser()
-        })
+        afterEach(async () => _afterEach())
 
     })
 
